aa6afc32479fd8bed19721369105e88b166f552d
IhorZhuk <ihor.zhuk92@gmail.com>
2018-01-21 21:17:20 +0100

    Use es6 syntax in test sample

--- __tests__/app/components/about-test.js
+++ __tests__/app/components/about-test.js
@@ -1,18 +1,17 @@
 jest.dontMock('../../../src/app/components/about.jsx');
 
-var React = require('react');
-var TestUtils = require('react-dom/test-utils');
-var About = require('../../../src/app/components/about.jsx').default;
+import React from 'react';
+import TestUtils from 'react-dom/test-utils';
+import About from '../../../src/app/components/about.jsx';
 
    
-describe("components/about", function() {
+describe('components/about', () => {
 
-  var about = TestUtils.renderIntoDocument(<About/>);
+  const about = TestUtils.renderIntoDocument(<About/>);
 
-  it("should render 'ABOUT' title", function() {
-    var title = TestUtils.findRenderedDOMComponentWithTag(about,'h3');
+  it("should render 'ABOUT' title", () => {
+    const title = TestUtils.findRenderedDOMComponentWithTag(about,'h3');
     expect(title.textContent).toEqual('ABOUT')
-  });
-
+  })
 
 });
