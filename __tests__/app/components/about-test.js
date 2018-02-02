acd5d9335d3666bd47b0372f26009b96e6739126
IhorZhuk <ihor.zhuk@wazzupa.com>
2018-02-02 14:24:57 +0100

    Add enzyme. Update sample test

--- __tests__/app/components/about-test.js
+++ __tests__/app/components/about-test.js
@@ -1,7 +1,7 @@
-jest.dontMock('../../../src/app/components/about.jsx');
+jest.dontMock('Components/about.jsx');
 
 import React from 'react';
-import TestUtils from 'react-dom/test-utils';
+import { shallow } from 'enzyme';
 import {About} from 'Components/about.jsx';
 
    
@@ -9,16 +9,15 @@ describe('Components/about', () => {
   const props = {
     name: "Zora"
   }
-  const about = TestUtils.renderIntoDocument(<About {...props}/>);
+
+  const wrapper = shallow(<About {...props}/>);
 
   it("should render 'ABOUT' title", () => {
-    const title = TestUtils.findRenderedDOMComponentWithTag(about,'h3');
-    expect(title.textContent).toEqual('ABOUT')
+    expect(wrapper.find('h3').text()).toEqual('ABOUT');
   })
 
   it("should render passed name", () => {
-    const name = TestUtils.findRenderedDOMComponentWithTag(about,'h4');
-    expect(name.textContent).toEqual(props.name)
+    expect(wrapper.find('h4').text()).toEqual(props.name);
   })
 
 });
