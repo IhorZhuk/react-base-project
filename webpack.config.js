006875ea470f43c973d7c11a8bdb339b9f4ceedc
IhorZhuk <ihor.zhuk@wazzupa.com>
2019-02-22 16:01:45 +0100

    update package json and webpack congfig

--- webpack.config.js
+++ webpack.config.js
@@ -5,47 +5,47 @@ var DIST_DIR = path.resolve(__dirname, "dist");
 var SRC_DIR = path.resolve(__dirname, "src");
 var TEST_DIR = path.resolve(__dirname, "test");
 
-var config = {
-  entry: SRC_DIR + "/app/index.js",
-  output: {
-    path: DIST_DIR + "/app",
-    filename: "bundle.js",
-    publicPath: "/app",
-  },
-  resolve: {
-    alias: {
-      Components: SRC_DIR + '/app/components',
-      Reducers: SRC_DIR + '/app/reducers',
-      Controllers: SRC_DIR + '/app/controllers',
-      Pages: SRC_DIR + '/app/pages',
-      Test: TEST_DIR 
+var config = (env, argv) => {
+  return {
+    mode: argv.mode == 'production' ? 'production' :  'development',
+    entry: SRC_DIR + "/app/index.js",
+    output: {
+      path: DIST_DIR + "/app",
+      filename: "bundle.js",
+      publicPath: "/app",
     },
-    extensions: ['.js', '.jsx'] 
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        include: SRC_DIR,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["react", "es2015", "stage-2"]
-          }
-        }
+    resolve: {
+      alias: {
+        Components: SRC_DIR + '/app/components',
+        Reducers: SRC_DIR + '/app/reducers',
+        Controllers: SRC_DIR + '/app/controllers',
+        Pages: SRC_DIR + '/app/components/pages',
+        Test: TEST_DIR 
       },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
-      }
-    ] //rules
-  }, //module
-  plugins: [
-    new ExtractTextPlugin('app.css')
-  ]
+      extensions: ['.js', '.jsx'] 
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          include: SRC_DIR,
+          use: {
+            loader: "babel-loader",
+          }
+        },
+        {
+          test: /\.scss$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader', 'sass-loader']
+          })
+        }
+      ] //rules
+    }, //module
+    plugins: [
+      new ExtractTextPlugin('app.css')
+    ]
+  }
 };
 
 module.exports = config;
